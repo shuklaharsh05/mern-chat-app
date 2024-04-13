82356dfabc09de697dd4c7d4f1a813c22f2d266b
shuklaharsh05 <126856278+shuklaharsh05@users.noreply.github.com>
2024-04-13 14:18:56 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-7m8h.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
